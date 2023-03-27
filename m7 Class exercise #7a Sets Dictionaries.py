609df49e62e5c66bcdfa13451c19c0b99b3753aa
Jacqueline C <79559850+jjackiie@users.noreply.github.com>
2023-03-26 21:24:43 -0700

    class exercise 7a

---
+++ m7 Class exercise #7a Sets Dictionaries.py
@@ -0,0 +1,34 @@
+# ====================================
+# Attached: m7 Class Exercise #7a
+# ====================================
+# File: Challenge Exercise #1
+# ====================================
+# Name: Basketball
+# ====================================
+# Programmer: Jacqueline Ceballos
+# Class: CMPR 114
+# ====================================
+
+# this program demonstrates various set operations
+baseball = {"jodi", "carmen", "aida", "alicia"}
+basketball = {"eva", "carmen", "alicia", "sarah"}
+
+# display members of the baseball set
+print("The following students are on the baseball team: ")
+for name in baseball:
+    print(name)
+
+# display members of the basketball set.
+print()
+print("The following students are on the basketball team: ")
+for name in basketball:
+    print(name)
+
+# demonstrate intersection
+print()
+print("")
+''''
+=================== Output ===========================
+
+
+'''
